3235def5925ae4f9ae6d2c073887ddfa586187e2
Phoebe Kaye Lerog <p.lerog.524638@umindanao.edu.ph>
2023-12-09 13:44:54 +0800

    ambot

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:organica_project/Admin/login.dart';
-import 'package:organica_project/authenticator.dart';
-
+import 'package:organica_project/Admin/Add_Data.dart';
+import 'package:organica_project/photo.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -25,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Authenticator(),
+      home: const Images(),
     );
   }
 }
